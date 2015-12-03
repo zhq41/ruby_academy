e1c225e866613e371252bcfd4329ef5b27d573d4
zhq41 <ziaulhaq@mhs.dinus.ac.id>
2015-12-04 00:54:43 +0700

    tipe data dalam ruby

--- 001_typedata.rb
+++ 001_typedata.rb
@@ -1,4 +1,27 @@
 #Learn Ruby 001
 #Belajar tipe data
 
+=begin
+ secara umum ada beberapa tipe dasar (bahasanya begitu) :
+ 1). number
+ 2). string
+ 3). boolean
+ kita akan coba dengan contoh satu per satu...
+=end
 
+
+=begin
+ #NUMBER
+ ya isinya nomor contohnya sebagai berikut :
+=end
+
+varA = 10
+varB = 10.1
+
+=begin
+  contoh diatas berupa FIXNUM. jadi ada banyak macamnya, seperti :
+(integer) http://ruby-doc.org/core-2.2.3/Integer.html
+(fixnum ) http://ruby-doc.org/core-2.2.3/Fixnum.html
+(float  ) http://ruby-doc.org/core-2.2.3/Float.html
+(bignum ) http://ruby-doc.org/core-2.2.3/Bignum.html
+=end
